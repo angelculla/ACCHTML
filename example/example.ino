478a822d5e164c26a6e9614c2d2250323aa20b99
angelculla <angel_culla@yahoo.es>
2025-03-19 18:09:53 +0100

    Update example.ino

--- example/example.ino
+++ example/example.ino
@@ -1,6 +1,7 @@
 /**************************************************************
 Example code using acchtml library (HTML generator)
 
+- acchtml is a class derived from WebServer that adds methods to generate HTML code
 - All methods of acchtml uses absolute coordinates to locate their outputs
 - In the file graph.inc there are graphics that will be placed embedded in the bin code
   you can use any tool to dump the images to text file
@@ -11,7 +12,6 @@ Example code using acchtml library (HTML generator)
 
 
 #include <WiFi.h>
-#include <WebServer.h>
 #include <acchtml.h>
 #include "graph.inc"
 
@@ -35,7 +35,7 @@ String pwd="";
 void cgi_index() {                                                   // This is de default page
   String s;
 
-  html.begin("",0);                                                  // begins the html code without refresh by time
+  html.beginpage("",0);                                                  // begins the html code without refresh by time
   html.addimagelink("graph_google",20,20,"http://www.google.com");   // imagelink graph_google in coordinates (20,20)
   html.addlink("http://www.google.com","link to GOOGLE",20,120);     // normal link in (20,120)
   s="Values entered: ";
@@ -53,41 +53,41 @@ void cgi_index() {                                                   // This is 
   html.formpassword("Password:");                                     // input for password. the var name is fixet to "PWD"
   html.formend("Accept");                                             // ends the form with "Accept" in de submit button
   html.addimage("graph_pbs",500,100);                                 // normal image at (500,100)
-  html.end();                                                         // ends de html code and sends it
+  html.endpage();                                                         // ends de html code and sends it
 }
 
 
 
 void cgi_formok() {                                                  // This cgi is called by the submit of the form
-  input1=html.server.arg("v1");                                      // saves the received data into variables input1, input2 and pwd
-  input2=html.server.arg("v2");
-  pwd=html.server.arg("PWD");
+  input1=html.arg("v1");                                      // saves the received data into variables input1, input2 and pwd
+  input2=html.arg("v2");
+  pwd=html.arg("PWD");
   html.redirectto("/");                                              // redirects to the default page
 }
 
 
 
 void cgi_404() {                                                     // cgi called by error 404 (page not found)
-  html.server.send(404, "text/plain", "This page does not exist!");
+  html.send(404, "text/plain", "This page does not exist!");
 }
 
 
 
 void cgi_graph_google() {                                            // cgi called when the navigator asks for the file graph_google
-  html.server.send_P(200,"image/png",graph_google,sizeof(graph_google));
+  html.send_P(200,"image/png",graph_google,sizeof(graph_google));
 }
 
 
 
 void cgi_graph_pbs() {
-  html.server.send_P(200,"image/png",graph_pb,sizeof(graph_pb));     // cgi called when the navigator asks for the file graph_pbs
+  html.send_P(200,"image/png",graph_pb,sizeof(graph_pb));     // cgi called when the navigator asks for the file graph_pbs
 }
 
 
 
 void task_web (void * parameter ) {                                  // RTOS task to run the webserver
   for(;;) {
-    html.server.handleClient();
+    html.handleClient();
     vTaskDelay(pdMS_TO_TICKS(1));
   }
 }
@@ -111,12 +111,12 @@ void setup() {
   Serial.print("Local ESP32 IP: ");
   Serial.println(WiFi.localIP());
 
-  html.server.on("/",cgi_index);                                     // sets the supported pages to our management functions
-  html.server.on("/formok.cgi",cgi_formok);
-  html.server.on("/graph_google",cgi_graph_google);
-  html.server.on("/graph_pbs",cgi_graph_pbs);
-  html.server.onNotFound(cgi_404);                                   // starts the webserver on port 80
-  html.start(80);
+  html.on("/",cgi_index);                                     // sets the supported pages to our management functions
+  html.on("/formok.cgi",cgi_formok);
+  html.on("/graph_google",cgi_graph_google);
+  html.on("/graph_pbs",cgi_graph_pbs);
+  html.onNotFound(cgi_404);                                   // starts the webserver on port 80
+  html.begin(80);
 
   xTaskCreate(task_web,"task_web",50000,NULL,1,NULL);                // runs the webserver in a RTOS task
 }
